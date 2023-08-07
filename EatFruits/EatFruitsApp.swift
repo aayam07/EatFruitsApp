0e5d1957d0ec6152851dc2fc21d115b75936d207
Aayam Adhikari <thisisleo7@gmail.com>
2023-08-07 18:26:08 +0545

    Initial Commit

---
+++ EatFruits/EatFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatFruitsApp.swift
+//  EatFruits
+//
+//  Created by Aayam Adhikari on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EatFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
