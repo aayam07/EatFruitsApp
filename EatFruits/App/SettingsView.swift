e67384596f0c0b54a6cf2d0246e5540eb221a199
Aayam Adhikari <thisisleo7@gmail.com>
2023-08-14 17:45:46 +0545

    Implement first section of the Settings view with a reusable label view content using GroupBox.

--- EatFruits/App/SettingsView.swift
+++ EatFruits/App/SettingsView.swift
@@ -13,14 +13,45 @@ struct SettingsView: View {
     // to create properties that stores values provided to us externally like device's appearance, time zone, color scheme i.e the information that comes from the view's environment. Allows us to know whether this settings view is currently present in the screen and also to dismiss it immediately. This property is attached to the isPresented variable which is stored in the App's environment.
     
 //    @Environment(\.isPresented) var isPresented
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.dismiss) var dismiss  // to dismiss the settings sheet programatically
     
     //MARK: - BODY
     var body: some View {
         NavigationView{
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    Text("Hello")
+                    //MARK: - SECTION 1
+                    
+                    GroupBox {
+                        // content of the GroupBox
+                        Divider()
+                            .padding(.vertical, 4)
+                        
+                        HStack(alignment: .center, spacing: 10, content: {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Spacer()
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholestrol. Fruits are the sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        })
+                    } label: {
+                        // resuable Label View Component
+                       SettingsLabelView(labelText: "EatFruits", labelImage: "info.circle")
+                    }
+
+                    
+                    //MARK: - SECTION 2
+                    
+                    
+                    //MARK: - SECTION 3
+                    
+                    
+
                 }  //: VSTACK
                 .navigationTitle("Settings")
                 .navigationBarTitleDisplayMode(.large)
